7c6824f6cfdf3381894f5fd84e2b5014f9fd9893
saifulislammunna <saifulmunna28@gmail.com>
2022-02-01 16:26:55 +0600

    login test completed

--- SampleProjects/POMProjectDemo/Tests/login.py
+++ SampleProjects/POMProjectDemo/Tests/login.py
@@ -1,25 +1,39 @@
+import self as self
 from selenium import webdriver
 import time
+import unittest
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome(executable_path="c:\selenium-browser-driver\chromedriver")
+class LoginTest(unittest.TestCase):
 
-# driver.implicity_wait(10)
-# driver.maximize_window()
 
-driver.get("https://opensource-demo.orangehrmlive.com/")
-time.sleep(5)
-driver.find_element(By.XPATH, "//*[@id='txtUsername']").click()
-time.sleep(2)
-driver.find_element(By.XPATH, "//*[@id='txtUsername']").send_keys("Admin")
-time.sleep(5)
-driver.find_element(By.XPATH, "//*[@id='txtPassword']").click()
-time.sleep(2)
-driver.find_element(By.XPATH, "//*[@id='txtPassword']").send_keys("admin123")
-time.sleep(5)
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome(executable_path="c:\selenium-browser-driver\chromedriver")
+        # cls.driver.implicity_wait(10)
+        cls.driver.maximize_window()
 
-driver.find_element(By.XPATH, "//*[@id='btnLogin']").click()
 
-driver.close()
-driver.quit()
-print("Test completed")
+    def test_login_valid(self):
+        self.driver.get("https://opensource-demo.orangehrmlive.com/")
+        time.sleep(5)
+        self.driver.find_element(By.XPATH, "//*[@id='txtUsername']").click()
+        time.sleep(2)
+        self.driver.find_element(By.XPATH, "//*[@id='txtUsername']").send_keys("Admin")
+        time.sleep(5)
+        self.driver.find_element(By.XPATH, "//*[@id='txtPassword']").click()
+        time.sleep(2)
+        self.driver.find_element(By.XPATH, "//*[@id='txtPassword']").send_keys("admin123")
+        time.sleep(5)
+        self.driver.find_element(By.XPATH, "//*[@id='btnLogin']").click()
+        time.sleep(2)
+        self.driver.find_element(By.XPATH, "//*[@id='welcome']").click()
+        time.sleep(2)
+        self.driver.find_element(By.XPATH, "//*[@id='welcome-menu']/ul/li[3]/a").click()
+        time.sleep(2)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.close()
+        cls.driver.quit()
+        cls.print("Test completed")
