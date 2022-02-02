e74934bf50a1c55b27b40beeb814b1a17440a87e
saifulislammunna <saifulmunna28@gmail.com>
2022-02-02 10:54:32 +0600

    created a separate class for Locators

---
+++ SampleProjects/POMProjectDemo/Locators/locators.py
@@ -0,0 +1,10 @@
+class Locators():
+
+    #login page objects
+    username_textbox_id = "txtUsername"
+    password_textbox_id = "txtPassword"
+    login_button_id = "btnLogin"
+
+    #home page objects
+    welcome_link_id = "welcome"
+    logout_link_linkText = "Logout"
--- SampleProjects/POMProjectDemo/Pages/loginPage.py
+++ SampleProjects/POMProjectDemo/Pages/loginPage.py
@@ -1,15 +1,18 @@
+from SampleProjects.POMProjectDemo.Locators.locators import Locators
+
+
 class LoginPage():
 
     def __init__(self, driver):
         self.driver = driver
 
-        self.username_textbox_id = "txtUsername"
+        self.username_textbox_id = Locators.username_textbox_id
         self.password_textbox_id = "txtPassword"
         self.login_button_id = "btnLogin"
 
     def enter_username(self, username):
         self.driver.find_element_by_id(self.username_textbox_id).clear()
-        self.driver.find_element_by_id(self.username_textbox_id).send_keys(username)
+        self.driver.find_element_by_id(Locators.username_textbox_id).send_keys(username)
 
     def enter_password(self, password):
         self.driver.find_element_by_id(self.password_textbox_id).clear()
